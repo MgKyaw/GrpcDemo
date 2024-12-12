157cba25fdbb701f0ac59319b04c4d17a5dd3e07
MgKyaw <mgkyaw20112@gmail.com>
2024-12-12 23:38:07 +0700

    feat: Configure Client at Program.cs

--- GrpcDemo/Program.cs
+++ GrpcDemo/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GrpcDemo
 {
@@ -21,6 +16,10 @@ namespace GrpcDemo
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(svc =>
+                {
+                    svc.AddHostedService<Client>();
                 });
     }
 }
